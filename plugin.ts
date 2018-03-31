fe34af9a0cb78e7a17855f238c0f10fe76e4777e
Laura Lynn <laura@iys.io>
2018-03-31 21:51:05 +0200

    why is everything in spaces

--- plugin.ts
+++ plugin.ts
@@ -1,46 +1,46 @@
 import * as cli from 'cli-color';
 
-import {MikuiaClient} from '../mikuia-client/client'
+import {MikuiaClient} from '../mikuia-client/client';
 
-var Mikuia = new MikuiaClient('tcp://127.0.0.1', [3000, 3001]);
+var Mikuia = new MikuiaClient();
 Mikuia.connect();
 
 Mikuia.on('connected', () => {
-    console.log('Connected.');
-    Mikuia.identify('fun');
+	console.log('Connected.');
+	Mikuia.identify('fun');
 });
 
 Mikuia.on('identified', () => {
-    Mikuia.registerHandler('fun.roll', {
-        description: 'Rolls a dice.',
-        anonymous: true,
-        settings: {
-            limit: {
-                name: 'Limit',
-                description: 'Maximum number that can be rolled.',
-                default: 100,
-                type: 'number'
-            },
-            blockOverride: {
-                name: 'Block Overriding',
-                description: 'Don\'t let users provide their own roll limit.',
-                default: false,
-                type: 'boolean'
-            }
-        }
-    })
+	Mikuia.registerHandler('fun.roll', {
+		description: 'Rolls a dice.',
+		anonymous: true,
+		settings: {
+			limit: {
+				name: 'Limit',
+				description: 'Maximum number that can be rolled.',
+				default: 100,
+				type: 'number'
+			},
+			blockOverride: {
+				name: 'Block Overriding',
+				description: 'Don\'t let users provide their own roll limit.',
+				default: false,
+				type: 'boolean'
+			}
+		}
+	})
 });
 
 Mikuia.on('event:handler:fun.roll', (event) => {
-    var limit = event.settings.limit;
+	var limit = event.settings.limit;
 
-    if(event.tokens.length > 1 && !event.settings.blockOverride) {
-        limit = event.tokens[1];
-    }
+	if(event.tokens.length > 1 && !event.settings.blockOverride) {
+		limit = event.tokens[1];
+	}
 
-    var roll = Math.floor(Math.random() * limit);
+	var roll = Math.floor(Math.random() * limit);
 
-    Mikuia.respond(event, {
-        message: 'You rolled ' + roll + '.'
-    });
+	Mikuia.respond(event, {
+		message: 'You rolled ' + roll + '.'
+	});
 });
